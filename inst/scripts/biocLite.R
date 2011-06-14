6de638b796964fa09191866cc5641e8022dc43aa
Martin Morgan <mtmorgan@fhcrc.org>
2011-06-14 02:21:51 +0000

    obtain valid Bioc repository for BiocInstaller
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocInstaller@56157 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/biocLite.R
+++ inst/scripts/biocLite.R
@@ -14,13 +14,18 @@ local({
                  error=function(...) numeric_version(0.0))
     if (vers > "2.13" && biocVers > "2.8") {
         if (!suppressWarnings(require("BiocInstaller", quietly=TRUE))) {
-            reposFile <- file.path(R.home(), "etc", "repositories")
-            reposNames <- rownames(read.table(reposFile, sep="\t"))
-            idx <- match("BioCsoft", reposNames)
-            oldRepos <- setRepositories(ind=idx)
-            if (!is.null(oldRepos))
-                on.exit(options(oldRepos))
-            install.packages("BiocInstaller")
+            a <- NULL
+            p <- file.path(Sys.getenv("HOME"), ".R", "repositories")
+            if (file.exists(p)) {
+                a <- tools:::.read_repositories(p)
+                if (!"BioCsoft" %in% rownames(a)) 
+                    a <- NULL
+            }
+            if (is.null(a)) {
+                p <- file.path(R.home("etc"), "repositories")
+                a <- tools:::.read_repositories(p)
+            }
+            install.packages("BiocInstaller", repos=a["BioCsoft", "URL"])
             if (!suppressWarnings(require("BiocInstaller", quietly=TRUE))) {
                 stop("use 'install.packages(\"BiocInstaller\")' before\n",
                       "  'source(\"http://bioconductor.org/biocLite.R\")'")
