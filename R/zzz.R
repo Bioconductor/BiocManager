4c463dc362382c526a6289c08a37732b441908c2
Dan Tenenbaum <dtenenba@fhcrc.org>
2011-10-31 23:17:59 +0000

    depend on R >= 2.15.0, point to correct BIOC_VERSION and NEXT_R_DEVEL_VERSION.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocInstaller@59925 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 ## The following two values are updated with each bioC release
-BIOC_VERSION <- "2.9"            # this version of Bioconductor
-NEXT_R_DEVEL_VERSION <- "2.15.0" # next (not-yet-supported) version of R
+BIOC_VERSION <- "2.10"            # this version of Bioconductor
+NEXT_R_DEVEL_VERSION <- "2.16.0" # next (not-yet-supported) version of R
 
 
 ## Change when the status of MBNI changes. 
