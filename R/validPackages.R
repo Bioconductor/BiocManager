69fc6686d9213edb3b683a4fa1f90358cfd0d184
Martin Morgan <mtmorgan@fhcrc.org>
2013-03-29 00:37:21 +0000

    add validPackages function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocInstaller@75018 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/validPackages.R
@@ -0,0 +1,63 @@
+.tooNewPkgs <-
+    function(instPkgs, availPkgs)
+{
+    idx <- rownames(availPkgs) %in% rownames(instPkgs)
+    vers <- availPkgs[idx, "Version"]
+    tooNew <- names(vers)[vers < instPkgs[names(vers), "Version"]]
+    instPkgs[tooNew, c("Version", "LibPath"), drop=FALSE]
+}
+
+validPackages <-
+    function(pkgs = installed.packages(lib.loc, priority=priority),
+             lib.loc=NULL, priority="NA", type=getOption("pkgType"),
+             filters=NULL, silent=FALSE)
+{
+    if (!is.matrix(pkgs)) {
+        if (is.character(pkgs))
+            pkgs <- installed.packages(pkgs, lib.loc=lib.loc)
+        else
+            .stop("'pkgs' must be a character vector of package names,
+                   or a matrix like that returned by 'installed.packages()'")
+    }
+    repos <- biocinstallRepos()
+    contribUrl <- contrib.url(repos, type=type)
+
+    availPkgs <- available.packages(contribUrl, type=type, filters=filters)
+    oldPkgs <- old.packages(lib.loc, repos=biocinstallRepos(),
+        instPkgs=pkgs, available=availPkgs, checkBuilt=TRUE,
+        type=type)
+    tooNewPkgs <- .tooNewPkgs(pkgs, availPkgs)
+
+    result <- structure(list(oldPkgs=oldPkgs, tooNewPkgs = tooNewPkgs),
+                        class="validPackages")
+    valid <- (NROW(oldPkgs) == 0) && (NROW(tooNewPkgs) == 0)
+    if (!valid && !silent) {
+        print(result)
+        .stop("some packages are not valid")
+    }
+    valid
+}
+
+print.validPackages <-
+    function(x, ...)
+{
+    if (NROW(x$oldPkgs)) {
+        cat("\n* Out-of-date packages\n")
+        print(x$oldPkgs)
+        cat("\nupdate with biocLite()\n")
+    }
+
+    if (NROW(x$tooNewPkgs)) {
+        cat("\n* Packages too new for Bioconductor version ",
+            sQuote(as.character(biocVersion())), "\n\n", sep="")
+        print(x$tooNewPkgs)
+        pkgs <- paste(dQuote(rownames(x$tooNewPkgs)), collapse=", ")
+        msg <- .msg(ifelse(length(pkgs) == 1L, "biocLite(%s)",
+                           "biocLite(c(%s))"), pkgs)
+        cat("\ndowngrade with ", msg, "\n", sep="")
+    }
+
+    cat("\n* sessionInfo()\n\n")
+    print(sessionInfo())
+    cat("\n")
+}
