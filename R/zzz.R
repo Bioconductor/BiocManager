0006064bda91d286eef064323f68d1851d62683a
Dan Tenenbaum <dtenenba@fhcrc.org>
2014-10-13 22:16:48 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 5 commits.
    Commit information:
    
    Commit id: f2be302192574aeea3a8c4a97da3b2af6e935e60
    
        Don't release the final biocLite.R yet....
    
    Committed by: Dan Tenenbaum
    Author Name: Dan Tenenbaum
    Commit date: 2014-10-13 15:15:32 -0700
    Author date: 2014-10-13 15:15:32 -0700
    
    Commit id: 770f8a79f76944bf0788175043e5aa2b72abd871
    
        Merge branch '3.1-version'
    
    Committed by: Dan Tenenbaum
    Author Name: Dan Tenenbaum
    Commit date: 2014-10-13 15:12:50 -0700
    Author date: 2014-10-13 15:12:50 -0700
    
    Commit id: b8fabe473053007288837f38ebe95e571e2fd959
    
        depend on R (>= 3.2.0)
    
    Committed by: Dan Tenenbaum
    Author Name: Dan Tenenbaum
    Commit date: 2014-10-13 15:11:09 -0700
    Author date: 2014-10-13 15:11:09 -0700
    
    Commit id: d96f07a12b97a6e8795cb2ffcfc148870bd85cb7
    
        point current R to correct repostiory
    
    Committed by: mtmorgan
    Author Name: mtmorgan
    Commit date: 2014-10-10 10:45:29 -0700
    Author date: 2014-10-10 10:45:29 -0700
    
    Commit id: b9ada0edbc284da6661ccec8b1308dbcb1eebaae
    
        prepare for 3.0 release
    
    Committed by: Martin Morgan
    Author Name: Martin Morgan
    Commit date: 2014-10-08 13:30:07 -0700
    Author date: 2014-10-08 13:30:07 -0700
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocInstaller@95447 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 ## The following values are updated with each Bioc release; see .onLoad
-BIOC_VERSION <- package_version("3.0")    # Bioc version for this package
-R_VERSION <- package_version("3.1.0")      # R version for this package
+BIOC_VERSION <- package_version("3.1")    # Bioc version for this package
+R_VERSION <- package_version("3.2.0")      # R version for this package
 IS_USER <- FALSE                           # TRUE if this version of
                                            # Bioconductor is the
                                            # current release version
@@ -15,18 +15,18 @@ IS_UPGRADEABLE <- FALSE                    # TRUE if a more recent
                                            # version of R
 UPGRADE_IS_DEVEL <- TRUE                   # TRUE if UPGRADE_VERSION
                                            # is for devel use only
-IS_DOWNGRADEABLE <- TRUE                   # TRUE if an older version
+IS_DOWNGRADEABLE <- FALSE                  # TRUE if an older version
                                            # (release or devel) of
                                            # Bioconductor is available
                                            # for this version of R
-UPGRADE_VERSION <- package_version("3.1")  # Bioconductor version for
+UPGRADE_VERSION <- package_version("3.2")  # Bioconductor version for
                                            # upgrade, if
                                            # IS_UPGRADEABLE == TRUE
-DOWNGRADE_VERSION <- package_version("2.14") # Bioconductor verson for
+DOWNGRADE_VERSION <- package_version("3.0") # Bioconductor version for
                                            # downgrade, if
                                            # IS_DOWNGRADEABLE == # TRUE
 
-NEXT_R_DEVEL_VERSION <- "3.2.0" # next (not-yet-supported) version of R
+NEXT_R_DEVEL_VERSION <- "3.3.0" # next (not-yet-supported) version of R
 
 ## Change when the status of MBNI changes. 
 ## Make sure this change is propagated to users, even 
