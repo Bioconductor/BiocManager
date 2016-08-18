229dd02a77d760a2007ac8ca42bb1e334e516a03
Martin Morgan <mtmorgan@fhcrc.org>
2016-08-18 22:32:57 +0000

    *** empty log message ***
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BiocInstaller@120263 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getUpdateablePackages.R
+++
@@ -1,8 +0,0 @@
-filterPackagesToUpdate <-
-    function(regexes, pkgs)
-{
-    regex = paste(regexes, collapse="|")
-    hits <- grep(regex, pkgs[,"Package"], invert=TRUE)
-    pkgs[hits, , drop=FALSE]
-}
-
