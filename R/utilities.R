69f1c2ee068f9e632a748cd69502e2f311eb2535
LiNk-NY <mramos09@gmail.com>
2018-02-22 20:34:06 -0500

    move .getContribUrl, remove update helper functions

--- R/utilities.R
+++ R/utilities.R
@@ -60,3 +60,36 @@
     }
     paste(major, minor, sep=".")
 }
+
+.getContribUrl <-
+    function(biocVersion)
+{
+    .contribUrl <-
+        function(repos)
+    {
+        contribUrl <- contrib.url(repos)
+        pkgs <- available.packages(contribUrl)
+        if (nrow(pkgs) == 0L)
+            .stop("no packages in repository (no internet connection?)",
+                  call.=FALSE)
+        else if (!"BiocInstaller" %in% rownames(pkgs))
+            .stop("'BiocInstaller' package not in repository",
+                  call.=FALSE)
+        contribUrl
+    }
+    repos <- biocinstallRepos(version=biocVersion)["BioCsoft"]
+    suppressWarnings(tryCatch({
+        .contribUrl(repos)
+    }, error=function(err) {
+        version <- getRversion()
+        currentVersion <- sprintf("%d.%d", version$major, version$minor)
+        lowerVersion <- .lowerRVersionString(version)
+        oldRepos <- sub(currentVersion, lowerVersion, repos)
+        if (oldRepos == repos)
+            .stop("'%s' while trying %s", conditionMessage(err),
+                  repos, call.=FALSE)
+        .message("'%s' while trying %s, trying %s", conditionMessage(err),
+                 repos, oldRepos)
+        .contribUrl(oldRepos)
+    }))
+}
