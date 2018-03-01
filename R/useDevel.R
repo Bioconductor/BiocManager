ec5ab65e7061de7b49f2c36546669cc51269bcbc
LiNk-NY <mramos09@gmail.com>
2018-03-01 14:07:31 -0500

    update use* functions

--- R/useDevel.R
+++ R/useDevel.R
@@ -1,36 +1,31 @@
 isDevel <- function() !IS_USER
 
-useDevel <-
-    function(devel=TRUE)
+useDevel <- function()
 {
-    if (devel) {
-        if (isDevel())
-            .stop("'devel' version already in use")
-        if (IS_END_OF_LIFE)
-            .stop("'devel' version not available")
-        if (!IS_UPGRADEABLE)
-            .stop("'devel' version requires a more recent R")
-        biocVers <- UPGRADE_VERSION
-    } else {
-        if (!isDevel())
-            .stop("'devel' version not in use")
-        if (!IS_DOWNGRADEABLE)
-            .stop("'devel' version cannot be down-graded with this version of R")
-        biocVers <- DOWNGRADE_VERSION
-    }
-    .update(biocVers, FALSE)
+    if (isDevel())
+        .stop("'devel' version already in use")
+    if (!IS_UPGRADEABLE)
+        .stop("'devel' version requires a more recent R")
+
+    .update(UPGRADE_VERSION, FALSE)
 }
 
-useRelease <-
-    function(release=TRUE)
+useRelease <- function()
 {
-    if (release) {
-        if (isDevel())
-            ## prompt user
-    }
+    if (!isDevel())
+        .stop("'release' version already in use")
+
+    txt <- sprintf(
+        "Downgrade all packages to Bioconductor version %s? [y/n]: ",
+        DOWNGRADE_VERSION)
+    answer <- .getAnswer(txt, allowed = c("y", "Y", "n", "N"))
+    if ("y" == answer)
+        .update(DOWNGRADE_VERSION, TRUE)
 }
+
 .update <-
     function(biocVersion, biocLiteAfterUpdate = FALSE)
 {
 ## TODO: write mechanism to upgrade and downgrade packages
 }
+
