9945446d60a6504a2f260ae80ed113d31d489ef1
LiNk-NY <mramos09@gmail.com>
2018-03-02 13:05:20 -0500

    remove mentions of BiocUpgrade

--- R/zzz.R
+++ R/zzz.R
@@ -75,7 +75,7 @@ globalVariables("repos")           # used in 'bootstrap' functions
      if (IS_END_OF_LIFE) {
          if (IS_UPGRADEABLE)
              .message("A newer version of Bioconductor is available for
-                       this version of R, ?BiocUpgrade for help")
+                       this version of R, ?useRelease for help")
          else if (Rversion > R_VERSION_MAX)
              .warning("Bioconductor version %s is too old for R version %s;
                  see https://bioconductor.org/install/#troubleshoot-biocinstaller",
