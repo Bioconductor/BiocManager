bde9a4e1d9bd202cbb94398f1d4bbb13b6c12ede
LiNk-NY <mramos09@gmail.com>
2018-03-02 20:17:18 -0500

    convert useDevel test to use testthat

--- tests/testthat/test_useDevel.R
+++ tests/testthat/test_useDevel.R
@@ -1,26 +1,27 @@
-test_useDevel <- function()
-{
-    if (!Bioconductor:::IS_DOWNGRADEABLE)
-        checkException(useDevel(FALSE), silent=TRUE)
-    if (!Bioconductor:::IS_UPGRADEABLE) {
-        checkException(useDevel(), silent=TRUE)
-        opts <- options(warn=2); on.exit(options(opts))
-        checkException(install("BiocUpgrade"))
-    }
-}
+context("Selecting appropriate version of Bioconductor")
 
-test_getContribUrl_exist <- function()
-{
+test_that("useDevel works", {
+    if (!Bioconductor:::IS_DOWNGRADEABLE)
+        expect_error(useDevel(FALSE), silent=TRUE)
+    if (!Bioconductor:::IS_UPGRADEABLE) {
+        expect_error(useDevel(), silent=TRUE)
+        opts <- options(warn=2); on.exit(options(opts))
+        expect_error(install("BiocUpgrade"))
+    }
+})
+
+test_that("ContribUrl exists", {
+    skip("Bioconductor package not yet in repository")
     fun <- Bioconductor:::.getContribUrl
 
     vers <- Bioconductor:::BIOC_VERSION
-    checkTrue(grepl(vers, fun(vers)))
+    expect_true(grepl(vers, fun(vers)))
     if (Bioconductor:::IS_UPGRADEABLE) {
         vers <- Bioconductor:::UPGRADE_VERSION
-        checkTrue(grepl(vers, fun(vers)))
+        expect_true(grepl(vers, fun(vers)))
     }
     if (Bioconductor:::IS_DOWNGRADEABLE) {
         vers <- Bioconductor:::DOWNGRADE_VERSION
-        checkTrue(grepl(vers, fun(vers)))
+        expect_true(grepl(vers, fun(vers)))
     }
-}
+})
