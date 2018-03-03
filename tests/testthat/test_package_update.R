0be4988a1011b1da5a69fa1c5ff810180dc47652
LiNk-NY <mramos09@gmail.com>
2018-03-02 20:08:46 -0500

    convert filter tests

--- tests/testthat/test_package_update.R
+++ tests/testthat/test_package_update.R
@@ -1,32 +1,34 @@
-test_package_filter_suppress_updates <- function() {
+context("Make use of various package type filters")
+
+testthat("suppressUpdates filter works" {
     .filter <- Bioconductor:::.package_filter_suppress_updates
 
     pkgs0 <- matrix(
         character(), 0, 2,
         dimnames=list(NULL, c("Package", "LibPath")))
 
-    checkIdentical(pkgs0, .filter(pkgs0, FALSE))
-    checkIdentical(pkgs0, .filter(pkgs0, character()))
+    expect_identical(pkgs0, .filter(pkgs0, FALSE))
+    expect_identical(pkgs0, .filter(pkgs0, character()))
 
     p0 <- tempdir()
     pkgs <- matrix(
         c("Foo", "Bar", "Baz", p0, p0, p0), 3, 2,
         dimnames=list(c("Foo", "Bar", "Baz"), c("Package", "LibPath")))
 
-    checkIdentical(pkgs, .filter(pkgs, FALSE))
-    checkIdentical(pkgs[3,,drop=FALSE], .filter(pkgs, c("Foo", "Bar")))
-    checkIdentical(pkgs0, .filter(pkgs, c("Foo", "Bar", "Baz")))
-    checkIdentical(pkgs0, .filter(pkgs, ""))
-}
+    expect_identical(pkgs, .filter(pkgs, FALSE))
+    expect_identical(pkgs[3,,drop=FALSE], .filter(pkgs, c("Foo", "Bar")))
+    expect_identical(pkgs0, .filter(pkgs, c("Foo", "Bar", "Baz")))
+    expect_identical(pkgs0, .filter(pkgs, ""))
+})
 
-test_package_filter_masked <- function() {
+test_that("masked packages are filtered", {
     .filter <- Bioconductor:::.package_filter_masked
 
     pkgs0 <- matrix(
         character(), 0, 2,
         dimnames=list(NULL, c("Package", "LibPath")))
 
-    checkIdentical(pkgs0, .filter(pkgs0))
+    expect_identical(pkgs0, .filter(pkgs0))
 
     paths <- c(tempfile(), tempfile())
     for (path in paths) dir.create(path)
@@ -37,20 +39,19 @@ test_package_filter_masked <- function() {
     pkgs <- matrix(
         c("Foo", "Bar", "Baz", "Bim", paths, paths), 4, 2,
         dimnames=list(c("Foo", "Bar", "Baz", "Bim"), c("Package", "LibPath")))
-    checkIdentical(pkgs, .filter(pkgs))
-    checkIdentical(pkgs[c(1, 3, 2),], .filter(pkgs[c(1, 3, 2),]))
+    expect_identical(pkgs, .filter(pkgs))
+    expect_identical(pkgs[c(1, 3, 2),], .filter(pkgs[c(1, 3, 2),]))
 
     pkgs <- matrix(
         c("Foo", "Bar", "Foo", paths, paths[2]), 3, 2,
         dimnames=list(c("Foo", "Bar", "Foo"), c("Package", "LibPath")))
-    checkIdentical(pkgs[1:2,], .filter(pkgs))
+    expect_identical(pkgs[1:2,], .filter(pkgs))
     pkgs <- pkgs[3:1,]
-    checkIdentical(pkgs[2:3,], .filter(pkgs))
-}
+    expect_identical(pkgs[2:3,], .filter(pkgs))
+})
 
-test_package_filter_unwriteable <- function() {
+test_that("unwriteable packages are not considered", {
     .filter <- Bioconductor:::.package_filter_unwriteable
-
     ## setup
     dir.create(p0 <- tempfile())
     on.exit(unlink(p0, recursive=TRUE))
@@ -60,31 +61,31 @@ test_package_filter_unwriteable <- function() {
         dimnames=list(NULL, c("Package", "LibPath")))
 
     pkgs <- pkgs0
-    checkIdentical(pkgs, .filter(pkgs, NULL))
-    checkIdentical(pkgs, .filter(pkgs, character()))
-    checkIdentical(pkgs, .filter(pkgs, tempdir()))
+    expect_identical(pkgs, .filter(pkgs, NULL))
+    expect_identical(pkgs, .filter(pkgs, character()))
+    expect_identical(pkgs, .filter(pkgs, tempdir()))
 
     pkgs <- matrix(c("Foo", p0), 1, byrow=TRUE,
                    dimnames=list("Foo", c("Package", "LibPath")))
-    checkIdentical(pkgs, .filter(pkgs, NULL))
-    checkIdentical(pkgs, .filter(pkgs, p0))
+    expect_identical(pkgs, .filter(pkgs, NULL))
+    expect_identical(pkgs, .filter(pkgs, p0))
 
     p1 <- tempfile()
     pkgs <- matrix(c("Foo", p1), 1, byrow=TRUE,
                    dimnames=list("Foo", c("Package", "LibPath")))
-    checkIdentical(pkgs[FALSE,, drop=FALSE], .filter(pkgs, NULL))
-    checkIdentical(pkgs[FALSE,, drop=FALSE], .filter(pkgs, p1))
-    checkIdentical(pkgs, .filter(pkgs, p0))
+    expect_identical(pkgs[FALSE,, drop=FALSE], .filter(pkgs, NULL))
+    expect_identical(pkgs[FALSE,, drop=FALSE], .filter(pkgs, p1))
+    expect_identical(pkgs, .filter(pkgs, p0))
 
     pkgs <- matrix(
         c("Foo", p0, "Bar", p1, "Baz", p0), 3, 2, byrow=TRUE,
         dimnames=list(c("Foo", "Bar", "Baz"), c("Package", "LibPath")))
-    checkIdentical(pkgs[c(1, 3),], .filter(pkgs, NULL))
-    checkIdentical(pkgs, .filter(pkgs, p0))
-    checkIdentical(pkgs0, .filter(pkgs, p1))
+    expect_identical(pkgs[c(1, 3),], .filter(pkgs, NULL))
+    expect_identical(pkgs, .filter(pkgs, p0))
+    expect_identical(pkgs0, .filter(pkgs, p1))
 
     msg <- tryCatch(.filter(pkgs, NULL), message=conditionMessage)
-    checkIdentical(
+    expect_identical(
         "installation path not writeable, unable to update packages: Bar\n",
         msg)
 
@@ -95,23 +96,23 @@ test_package_filter_unwriteable <- function() {
     dir.create(p2 <- tempfile(), mode="0400") # read but not write
     pkgs <- matrix(c("Foo", p2), 1, byrow=TRUE,
                    dimnames=list("Foo", c("Package", "LibPath")))
-    checkIdentical(pkgs0, .filter(pkgs, NULL))
+    expect_identical(pkgs0, .filter(pkgs, NULL))
 
     pkgs <- matrix(
         c("Foo", p0, "Bar", p2, "Baz", p0), 3, 2, byrow=TRUE,
         dimnames=list(c("Foo", "Bar", "Baz"), c("Package", "LibPath")))
-    checkIdentical(pkgs[c(1, 3),], .filter(pkgs, NULL))
-    checkIdentical(pkgs0, .filter(pkgs, p2))
+    expect_identical(pkgs[c(1, 3),], .filter(pkgs, NULL))
+    expect_identical(pkgs0, .filter(pkgs, p2))
 
     Sys.chmod(p2, mode="0700")
     unlink(p2, recursive=TRUE)
-}
+})
 
-test_writeable_install <- function() {
-    DEACTIVATED("too idiosyncratic for standardized testing")
+test_that("packages can be written", {
+    skip("too idiosyncratic for standardized testing")
 
-    lib <- system.file(package="Bioconductor", "unitTests", "cases",
-                       "lib", "Biobase")
+    lib <- system.file(package="Bioconductor", "tests", "cases",
+        "lib", "Biobase")
     dir.create(locked <- tempfile())
     file.copy(lib, locked, recursive=TRUE)
 
@@ -120,6 +121,6 @@ test_writeable_install <- function() {
     .libPaths(c(locked, .libPaths()))
 
     Sys.chmod(locked, mode="0500")
-    install()                          #
+    install()
     Sys.chmod(locked, mode="0700")
-}
+})
