5b39fec999d0f20bba4048789ee70df6d86c8f73
Martin Morgan <martin.morgan@roswellpark.org>
2018-03-17 06:27:02 -0400

    rename biocRepos() as repositories()

--- R/repositories.R
+++ R/repositories.R
@@ -3,18 +3,17 @@
 #'
 #' Display current Bioconductor and CRAN repositories.
 #'
-#'
 #' Displays the URLs of the repositories used by \code{\link{install}} to
 #' install Bioconductor and CRAN packages.
 #'
-#'
-#' @param siteRepos (Optional) \code{character(1)} representing an additional
-#'   repository (e.g., a URL to an organization's internally maintained
-#'   repository) in which to look for packages to install. This repository will
-#'   be prepended to the default repositories returned by the function.
-#' @param version (Optional) \code{character(1)} or \code{package_version}
-#'   indicating the Bioconductor version (e.g., \dQuote{3.1}) for which
-#'   repositories are required.
+#' @param site_repository (Optional) \code{character(1)} representing
+#'     an additional repository (e.g., a URL to an organization's
+#'     internally maintained repository) in which to look for packages
+#'     to install. This repository will be prepended to the default
+#'     repositories returned by the function.
+#' @param version (Optional) \code{character(1)} or
+#'     \code{package_version} indicating the Bioconductor version
+#'     (e.g., \dQuote{3.1}) for which repositories are required.
 #' @return Named \code{character()} of repositories.
 #' @seealso
 #'
@@ -27,21 +26,27 @@
 #'
 #' \code{\link{chooseCRANmirror}} lets you choose from a list of all public
 #'   CRAN mirror URLs.
+#'
 #' @keywords environment
+#'
 #' @examples
+#' repositories()
 #'
-#' installRepos()
-#'
-#' ## Choose mirrors
+#' ## Choose CRAN mirrors
 #' \dontrun{
 #'   chooseCRANmirror()
-#'   chooseBioCmirror()
 #' }
 #'
-#' @export installRepos
+#' @export repositories
 repositories <-
-    function(siteRepos=character(), version=version())
+    function(site_repository = character(), version = Bioconductor::version())
 {
+    version <- package_version(version)
+    stopifnot(
+        length(site_repository) <= 1L,
+        is.character(site_repository), !any(is.na(site_repository))
+    )
+
     biocVersion <- package_version(version)
 
     old.repos <- getOption("repos")
@@ -79,10 +84,11 @@ repositories <-
     repos <- getOption("repos")
 
     biocMirror <- getOption("BioC_mirror", "https://bioconductor.org")
-    biocPaths <- c(BioCsoft="bioc", BioCann="data/annotation",
-                    BioCexp="data/experiment")#, BioCworkflows="workflows")
-    biocRepos <- paste(biocMirror, "packages", biocVersion,
-                        biocPaths, sep="/")
+    biocPaths <- c(
+        BioCsoft="bioc", BioCann="data/annotation", BioCexp="data/experiment"
+        ##, BioCworkflows="workflows")
+    )
+    biocRepos <- paste(biocMirror, "packages", biocVersion, biocPaths, sep="/")
     repos[names(biocPaths)] <- biocRepos
 
     keepNames <- names(repos)[repos %in% old.repos]
@@ -90,41 +96,20 @@ repositories <-
 
     repos <- repos[keepRepos]
 
-    ## This needs to be commented out a few months (3? 4?) after the
-    ## next development cycle has started, when we are confident that
-    ## no developper is still using an early R devel with a
-    ## tools:::.BioC_version_associated_with_R_version still pointing
-    ## to the release repository.
-    if (isDevel())
-    {
-        ## comment repos here as they become available.
-        inactive <- c(
-                      ##   "BioCsoft"
-                      ## , "BioCann"
-                      ## , "BioCexp"
-                      )
-        DOWNGRADE_VERSION <-
-            BiocVersion:::.subtractVersion(version(), 1L)
-        ## No need to touch below.
-        tmpRepos <- paste(biocMirror, "packages", DOWNGRADE_VERSION,
-                          biocPaths[inactive], sep="/")
-        repos[inactive] <- tmpRepos
-    }
-
     CRAN_repo <- repos[["CRAN"]]
     ## Microsoft R Open is shipped with getOption("repos")[["CRAN"]]
     ## pointing to a *snapshot* of CRAN (e.g.
-    ## https://mran.microsoft.com/snapshot/2017-05-01), and not
-    ## to a CRAN mirror that is current. For the current release and devel
-    ## BioC versions, installRepos() needs to point to a CRAN mirror
+    ## https://mran.microsoft.com/snapshot/2017-05-01), and not to a
+    ## CRAN mirror that is current. For the current release and devel
+    ## BioC versions, repositories() needs to point to a CRAN mirror
     ## that is current so install() and valid() behave the same for
-    ## all BioC users, whether they use mainstream R or Microsoft R Open.
-    ## However, since old versions of BioC are frozen, it would probably
-    ## make sense to point to a *snapshot* of CRAN instead of a CRAN mirror
-    ## that is current.
+    ## all BioC users, whether they use mainstream R or Microsoft R
+    ## Open.  However, since old versions of BioC are frozen, it would
+    ## probably make sense to point to a *snapshot* of CRAN instead of
+    ## a CRAN mirror that is current.
     snapshot_pattern <- "/snapshot/[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9]"
     if (CRAN_repo == "@CRAN@" || grepl(snapshot_pattern, CRAN_repo))
         repos[["CRAN"]] <- "https://cran.rstudio.com"
 
-    c(siteRepos=siteRepos, repos)
+    c(site_repository = site_repository, repos)
 }
