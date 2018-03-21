8f2329f5a35fac380152a66e43e15182c9091504
LiNk-NY <mramos09@gmail.com>
2018-03-21 18:40:00 -0400

    replace version checker with helper function
    - .version_diagnosis returns TRUE when OK
    - otherwise, text is returned

--- R/zzz.R
+++ R/zzz.R
@@ -3,29 +3,7 @@
 {
     .message("Bioconductor version %s (package version %s), ?install for help",
              version(), packageVersion("Bioconductor"))
-     Rversion <- getRversion()
-     if (IS_END_OF_LIFE) {
-         if (IS_UPGRADEABLE)
-             .message('
-                 A newer version of Bioconductor is available for
-                 this version of R, see vignettes(package="Bioconductor")
-                 for help
-             ')
-         else if (Rversion > R_VERSION_MAX)
-             .warning("Bioconductor version %s is too old for R version %s;
-                 see https://bioconductor.org/install/#troubleshoot-biocinstaller",
-                 BIOC_VERSION, Rversion)
-         else
-             .message("A new version of Bioconductor is available after
-                       installing the most recent version of R; see
-                       http://bioconductor.org/install")
-     } else if (Rversion > R_VERSION_MAX) {
-         if (Rversion >= NEXT_R_DEVEL_VERSION)
-             .message("Bioconductor does not yet support R version %s", Rversion)
-         else
-             .warning("Bioconductor version %s is too old for R version %s;
-                 see https://bioconductor.org/install/#troubleshoot-biocinstaller",
-                 BIOC_VERSION, Rversion)
-     }
-
+    ver_result <- .version_diagnosis()
+    if (!isTRUE(ver_result))
+        ver_result
 }
