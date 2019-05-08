c9a1d61e41e746f9b32897d3a8c2afd421303b8e
LiNk-NY <mramos09@gmail.com>
2019-05-08 19:09:44 -0400

    Informative message refers to #47

--- R/install.R
+++ R/install.R
@@ -350,7 +350,11 @@ install <-
         npkgs <- .install_n_invalid_pkgs(valist) + length(pkgs)
         if (!length(pkgs)-1L) {
             .install_ask_up_or_down_grade(version, npkgs, cmp, ask) ||
-                .stop("Bioconductor version not changed")
+                .stop(
+                    paste0("'install()' failed in non-interactive session;",
+                        "force installation with 'install(..., ask = FALSE)'"
+                    )
+                )
         } else {
             fmt <- paste0(c(
                 "To use Bioconductor version '%s', first %s %d packages with",
