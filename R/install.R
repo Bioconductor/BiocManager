a0216014171b98d1e671ba56cc3dbaf4313bf97f
Martin Morgan <martin.morgan@roswellpark.org>
2019-05-10 06:26:05 -0400

    suggest ask = FALSE only when !interactive()

--- R/install.R
+++ R/install.R
@@ -350,10 +350,11 @@ install <-
         npkgs <- .install_n_invalid_pkgs(valist) + length(pkgs)
         if (!length(pkgs)-1L) {
             .install_ask_up_or_down_grade(version, npkgs, cmp, ask) ||
-                .stop(
-                    "'install()' failed in non-interactive session;
-                    force installation with 'install(..., ask = FALSE)'"
-                )
+                .stop(paste0(
+                    "Bioconductor version not changed by 'install()'",
+                    if (!interactive() && isTRUE(ask))
+                        "; in non-interactive sessions use 'ask = FALSE'"
+                ))
         } else {
             fmt <- paste0(c(
                 "To use Bioconductor version '%s', first %s %d packages with",
