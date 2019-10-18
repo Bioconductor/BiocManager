5a25b72c3ac141d82cf50b122ada646e1ae6b17e
Martin Morgan <martin.morgan@roswellpark.org>
2019-10-18 09:35:52 -0400

    make tests robust to uninstalled 'Suggests' BiocVersion

--- tests/testthat.R
+++ tests/testthat.R
@@ -4,6 +4,9 @@ library(BiocManager)
 .skip_if_misconfigured <-
     function()
 {
+    if (!"BiocVersion" %in% rownames(installed.packages()))
+        return(NULL)
+
     R_version <- getRversion()
     bioc_version <- packageVersion("BiocVersion")[, 1:2]
     if (R_version == "4.0.0" && bioc_version == "3.9")
