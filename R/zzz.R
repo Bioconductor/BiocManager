283fc306dc0b0d6e9c4a2649e9c22083f3437da2
Martin Morgan <martin.morgan@roswellpark.org>
2019-10-20 12:58:09 -0400

    report BiocManager & Bioc version only onLoad
    - don't check for valid configuration at load time

--- R/zzz.R
+++ R/zzz.R
@@ -1,28 +1,13 @@
-.onLoad <-
+.onAttach <-
     function(libname, pkgname)
 {
-    if (!interactive())
-        return()
-
     version <- version()
-    if (identical(version, .VERSION_SENTINEL)) {
-        .warning(.VERSION_UNKNOWN)
-        return()
-    }
-
     valid <- .version_validity(version)
-    isTRUE(valid) || ifelse(.is_CRAN_check(), .message(valid), .stop(valid))
-
-    fmt <- paste0(
-        "Bioconductor version %s (BiocManager %s), ",
-        "?BiocManager::install for help"
-    )
-    .packageStartupMessage(fmt, version, packageVersion("BiocManager"))
-
-    txt <- .version_is_not_future(version)
-    isTRUE(txt) || identical(txt, .VERSION_MAP_UNABLE_TO_VALIDATE) ||
-        .packageStartupMessage(txt)
-
-    recommend <- .version_recommend(version)
-    isTRUE(recommend) || .packageStartupMessage(recommend)
+    if (isTRUE(valid)) {
+        fmt <- paste0(
+            "Bioconductor version %s (BiocManager %s), ",
+            "?BiocManager::install for help"
+        )
+        .packageStartupMessage(fmt, version, packageVersion("BiocManager"))
+    }
 }
