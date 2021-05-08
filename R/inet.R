Bioconductor/BiocManager

commit d9a8392f01a183e28282190048ec908d30c552ab

add catch for warning and return more informative message, closes #85

R/inet.R
@@ -85,6 +85,13 @@ NULL
         }, error = function(e) {
             .inet_error(e)
             invisible(NULL)
+        }, warning = function(w) {
+            msg <- conditionMessage(w)
+            if (grepl("not available", msg))
+                msg <- gsub("this version of R",
+                    paste0("Bioconductor version ", "'", version(), "'"),
+                    msg)
+            .warning(msg)
         })
     }, warning = .inet_warning)
 }        

commit dd2b7fcc6fb94cc0c86c26cd6031ea846149f18e

fix #100 BiocManager::install() fails for binary repositories
- commit d9a8392f01a183e28282190048ec908d30c552ab introduced a bug
  where warnings in install.packages() were treated as errors, so
  clean-up code in available.packages() did not run. This updates the
  code to treat warnings as warnings

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: BiocManager
 Title: Access the Bioconductor Project Package Repository
 Description: A convenient tool to install and update Bioconductor packages.
-Version: 1.30.13
+Version: 1.30.13.1
 Authors@R: c(
     person("Martin", "Morgan", email = "martin.morgan@roswellpark.org",
         role = "aut", comment = c(ORCID = "0000-0002-5874-8148")),

R/inet.R
@@ -85,16 +85,18 @@ NULL
         }, error = function(e) {
             .inet_error(e)
             invisible(NULL)
-        }, warning = function(w) {
-            msg <- conditionMessage(w)
-            if (grepl("not available", msg))
-                msg <- gsub("this version of R",
-                    paste0("Bioconductor version ", "'", version(), "'"),
-                    msg)
-            .warning(msg)
         })
-    }, warning = .inet_warning)
-}        
+    }, warning = function(w) {
+        msg <- conditionMessage(w)
+        if (grepl("not available", msg))
+            msg <- gsub(
+                "this version of R",
+                paste0("Bioconductor version ", "'", version(), "'"),
+                msg
+            )
+        .inet_warning(msg)
+    })
+}
 
 .inet_old.packages <-
     function(...)